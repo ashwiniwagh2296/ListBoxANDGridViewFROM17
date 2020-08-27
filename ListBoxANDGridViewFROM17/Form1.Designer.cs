9385375b81cb327f17eaab3b04a9b6ca2033671c
ashwiniwagh2296 <ashwiniwagh2296@gmail.com>
2020-08-27 13:38:19 +0530

    Add project files.

---
+++ ListBoxANDGridViewFROM17/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ListBoxANDGridViewFROM17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Remove = new System.Windows.Forms.Button();
+            this.btn_AddAll = new System.Windows.Forms.Button();
+            this.btn_RemoveAll = new System.Windows.Forms.Button();
+            this.btn_Finalize = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(308, 238);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(528, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(300, 238);
+            this.listBox2.TabIndex = 1;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn_Add.Location = new System.Drawing.Point(354, 12);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(118, 42);
+            this.btn_Add.TabIndex = 2;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_Remove
+            // 
+            this.btn_Remove.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Remove.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn_Remove.Location = new System.Drawing.Point(354, 60);
+            this.btn_Remove.Name = "btn_Remove";
+            this.btn_Remove.Size = new System.Drawing.Size(118, 42);
+            this.btn_Remove.TabIndex = 3;
+            this.btn_Remove.Text = "Remove";
+            this.btn_Remove.UseVisualStyleBackColor = true;
+            this.btn_Remove.Click += new System.EventHandler(this.btn_Remove_Click);
+            // 
+            // btn_AddAll
+            // 
+            this.btn_AddAll.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_AddAll.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn_AddAll.Location = new System.Drawing.Point(354, 108);
+            this.btn_AddAll.Name = "btn_AddAll";
+            this.btn_AddAll.Size = new System.Drawing.Size(118, 42);
+            this.btn_AddAll.TabIndex = 4;
+            this.btn_AddAll.Text = "Add All";
+            this.btn_AddAll.UseVisualStyleBackColor = true;
+            this.btn_AddAll.Click += new System.EventHandler(this.btn_AddAll_Click);
+            // 
+            // btn_RemoveAll
+            // 
+            this.btn_RemoveAll.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_RemoveAll.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn_RemoveAll.Location = new System.Drawing.Point(354, 156);
+            this.btn_RemoveAll.Name = "btn_RemoveAll";
+            this.btn_RemoveAll.Size = new System.Drawing.Size(118, 42);
+            this.btn_RemoveAll.TabIndex = 5;
+            this.btn_RemoveAll.Text = "Remove All";
+            this.btn_RemoveAll.UseVisualStyleBackColor = true;
+            this.btn_RemoveAll.Click += new System.EventHandler(this.btn_RemoveAll_Click);
+            // 
+            // btn_Finalize
+            // 
+            this.btn_Finalize.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Finalize.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btn_Finalize.Location = new System.Drawing.Point(354, 204);
+            this.btn_Finalize.Name = "btn_Finalize";
+            this.btn_Finalize.Size = new System.Drawing.Size(118, 42);
+            this.btn_Finalize.TabIndex = 6;
+            this.btn_Finalize.Text = "Finalize";
+            this.btn_Finalize.UseVisualStyleBackColor = true;
+            this.btn_Finalize.Click += new System.EventHandler(this.btn_Finalize_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 309);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(816, 185);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 497);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_Finalize);
+            this.Controls.Add(this.btn_RemoveAll);
+            this.Controls.Add(this.btn_AddAll);
+            this.Controls.Add(this.btn_Remove);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Remove;
+        private System.Windows.Forms.Button btn_AddAll;
+        private System.Windows.Forms.Button btn_RemoveAll;
+        private System.Windows.Forms.Button btn_Finalize;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ ListBoxANDGridViewFROM17/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBoxANDGridViewFROM17
+{
+    public partial class Form1 : Form
+    {
+        private DataTable dtcourses = new DataTable();
+        private DataTable dtSelectedcourses = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillCoursesTable();
+            SelectedCoursesTable();
+
+            listBox1.DataSource = dtcourses;
+            listBox1.DisplayMember = "coursesName";
+            listBox2.DataSource = dtSelectedcourses;
+            listBox2.DisplayMember = "coursesName";
+        }
+
+        private void FillCoursesTable()
+        {
+            dtcourses.Columns.Add("coursesID", typeof(int));
+            dtcourses.Columns.Add("coursesName");
+            dtcourses.Columns.Add("coursesDuretion");
+
+
+            dtcourses.Rows.Add(1, "Advance OOP", "7 months");
+            dtcourses.Rows.Add(2, " OOP", "5 months");
+            dtcourses.Rows.Add(3, "Formal Methods", "5 months");
+            dtcourses.Rows.Add(4, " ITC", "7 months");
+            dtcourses.Rows.Add(5, " SQL", "3 months");
+            dtcourses.Rows.Add(6, "java", "4 months");
+            dtcourses.Rows.Add(7, "web desiging", "3 months");
+            dtcourses.Rows.Add(8, "Advance java", "8 months");
+        }
+        private void SelectedCoursesTable()
+        {
+            dtSelectedcourses.Columns.Add("coursesID", typeof(int));
+            dtSelectedcourses.Columns.Add("coursesName");
+            dtSelectedcourses.Columns.Add("coursesDuretion");
+
+
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            if(listBox1.Items.Count>0)
+            {
+                dtSelectedcourses.ImportRow(dtcourses.Rows[listBox1.SelectedIndex]);
+                dtcourses.Rows[listBox1.SelectedIndex].Delete();
+            }
+        }
+
+        private void btn_Remove_Click(object sender, EventArgs e)
+        {
+            if (listBox2.Items.Count > 0)
+            {
+                dtcourses.ImportRow(dtSelectedcourses.Rows[listBox2.SelectedIndex]);
+                dtSelectedcourses.Rows[listBox2.SelectedIndex].Delete();
+            }
+        }
+
+        private void btn_AddAll_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                int count = dtcourses.Rows.Count;
+                for (int i = count - 1; i >= 0; i--)
+                {
+                    dtSelectedcourses.ImportRow(dtcourses.Rows[listBox1.SelectedIndex]);
+                    dtcourses.Rows[listBox1.SelectedIndex].Delete();
+                }
+            }
+        }
+
+        private void btn_RemoveAll_Click(object sender, EventArgs e)
+        {
+            if (listBox2.Items.Count > 0)
+            {
+                int count = dtSelectedcourses.Rows.Count;
+                for (int i = count - 1; i >= 0; i--)
+                {
+                    dtcourses.ImportRow(dtSelectedcourses.Rows[listBox2.SelectedIndex]);
+                    dtSelectedcourses.Rows[listBox2.SelectedIndex].Delete();
+                }
+            }
+        }
+
+        private void btn_Finalize_Click(object sender, EventArgs e)
+        {
+         DialogResult dr=  MessageBox.Show("Are you sure you want to finalize the courses", "Confirmation message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if(dr==DialogResult.Yes)
+            {
+                dataGridView1.DataSource = dtSelectedcourses;
+
+                dataGridView1.Enabled = false;
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.RowHeadersVisible = false;
+                
+
+                dataGridView1.Columns[1].Width = 400;
+                dataGridView1.Columns[2].Width = 500;
+            }
+            else
+            {
+                MessageBox.Show("Please select atleast one course.",  "Information  message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+        }
+    }
+}
